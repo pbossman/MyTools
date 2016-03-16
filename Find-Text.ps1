9e7a25165dc50ad8d34d884af5115cce68e02ea9
Phil Bossman <pbossman@gmail.com>
2016-03-16 19:05:09 -0400

    Update

---
+++ Find-Text.ps1
@@ -0,0 +1,41 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Find-Text
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]$Path,
+
+        # Param2 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [String]
+        $String
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+
+    }
+    End
+    {
+    }
+}
